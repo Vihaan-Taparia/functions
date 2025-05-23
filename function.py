6d8b340edcd9b283b865633c28faa8d41d99931c
Vihaan-Taparia <mayataparia@gmail.com>
2025-05-23 21:14:05 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,15 @@
+#well wishes
+def well_wishes():
+    print("Hello")
+    print("How are you")
+
+well_wishes()
+
+#weather condition
+def weather_condition():
+    print('The weather is pleasant in:',spring)
+    print('The weather is same in:',autumn)
+spring="spring season"
+autumn="autumn season"
+weather_condition()
+
